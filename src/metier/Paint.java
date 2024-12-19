90580657fe846ec2d94d4081090fe4d10420fe19
Ottirate <sarah.hautot76@gmail.com>
2024-12-19 12:09:33 +0100

    Reprendre les dimensions du fond

--- src/metier/Paint.java
+++ src/metier/Paint.java
@@ -79,6 +79,7 @@ public class Paint
 	{
 		if (img != null)
 		{
+			// Taille par défauts
 			if (this.lstImages.isEmpty())
 			{
 				this.height = img.getImgHeight();
@@ -96,7 +97,7 @@ public class Paint
 			// Si on a changé la hauteur/largeur
 			if ( w == this.width && h == this.height) 
 				this.resizeBackground();
-				
+
 			this.save();
 		}
 
@@ -139,8 +140,8 @@ public class Paint
 				this.width = image.getImgWidth();
 		}
 
-		if (this.height == 0) this.height = DEFAULT_HEIGHT;
-		if (this.width  == 0) this.width  = DEFAULT_WIDTH;
+		if (this.height == 0) this.height = this.lstImages.get(0).getImgHeight();
+		if (this.width  == 0) this.width  = this.lstImages.get(0).getImgWidth();
 
 		this.resizeBackground();
 	}
